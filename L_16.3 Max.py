565bd239067c332e170b7598af0e758edbf542fe
23egih23 <egor.filatov@live.com>
2022-06-05 18:31:50 +0300

    Выбор max числа

---
+++ L_16.3 Max.py
@@ -0,0 +1,20 @@
+def myMAX(list1):
+    maxi = list1[0]
+    for i in list1:
+        if maxi<i:
+            maxi=i
+    return maxi
+
+a = [1,23,325,2,543,2,9]
+print(myMAX(a))
+
+def myMAX(list1):
+    maxi = list1[0]
+    for i in list1:
+        if maxi<i:
+            maxi=i
+    return maxi
+
+a = [1,23,325,2,543,2,9]
+print(myMAX(a))
+print(myMAX([1,2,3,4,5]))
